85ff6861ccbaa2c0daefe914aa3d0b31a92602ac
debbie <drosenfeld87@gmail.com>
2018-01-31 17:28:49 -0500

    complete exercise1 in exercises.rb

--- exercises.rb
+++ exercises.rb
@@ -7,5 +7,18 @@ artists = ["gnr", "metallica", "cancer bats"]
 colors_symbols = [:green, :blue, :black]
 
 words = { :cat => 'pet', :color => 'orange', :type => 'shorthair' }
-movies = { :fearandloathing => 1998, :princessbride => 1987 :lordoftherings => 2000 }
+movies = { :fearandloathing => 1998, :princessbride => 1987, :lordoftherings => 2000 }
 cities = { :toronto => 2000000, :newyork => 8000000, :london => 9000000 }
+my_people = { :devin => 30, :dalia => 40, :megan => 28}
+
+#exercise 1
+# puts coin_flip
+
+# puts fav_colors.first
+
+# puts ages.sort
+
+# ages.push(0)
+# puts ages
+
+puts movies[:princessbride]
