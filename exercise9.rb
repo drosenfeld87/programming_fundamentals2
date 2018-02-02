5ad587a7fb978ff96a53d4c74aac01cec0893c34
debbie <drosenfeld87@gmail.com>
2018-02-02 12:34:43 -0500

    complete exercise9

--- exercise9.rb
+++ exercise9.rb
@@ -1,1 +1,19 @@
-students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
+# students = {:cohort1 => 34, :cohort2 => 42, :cohort3 => 22}
+#
+# students[:cohort4]=43
+# puts students
+# #
+# students.each do |k,v|
+#   puts "#{k}: #{v} students"
+# end
+#
+# puts students.keys
+#
+# new_students = students.each do |k,v|
+#     students[k] = 1.05 * v
+# end
+#
+# puts new_students
+#
+# new_students.delete(:cohort2)
+# puts new_students
