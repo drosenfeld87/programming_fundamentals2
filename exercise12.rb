d8e0493d7045ff28c190641ea5646844abf7c831
debbie <drosenfeld87@gmail.com>
2018-02-02 13:52:00 -0500

    start exercise12

---
+++ exercise12.rb
@@ -0,0 +1,14 @@
+toppings = 0
+total_pizzas = 0
+
+puts "How many pizzas do you want to order"
+  answer_pizzas = gets.chomp.to_i
+
+puts "How many toppings for pizza 1?"
+  answer_toppings = gets.chomp.to_i
+
+puts "You have ordered #{answer_pizzas} pizzas with #{answer_toppings} toppings"
+
+while answer_pizzas != 0
+  total_pizzas = num + 1
+puts "How many toppings for pizza #{}"
